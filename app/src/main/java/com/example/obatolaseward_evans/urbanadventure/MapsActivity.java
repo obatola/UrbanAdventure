1730f3f9d77aeb1f4550682ee7f0872f5fbd81ec
Oba Seward-Evans <oba.seward@gmail.com>
2017-02-17 16:54:17 -0500

    initialize map and move to WPI

--- app/src/main/java/com/example/obatolaseward_evans/urbanadventure/MapsActivity.java
+++ app/src/main/java/com/example/obatolaseward_evans/urbanadventure/MapsActivity.java
@@ -38,9 +38,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker in WPI and move the camera
+        LatLng wpi = new LatLng(42.2746, -71.8063);
+        mMap.addMarker(new MarkerOptions().position(wpi).title("Marker in WPI"));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(wpi, 16));
+
     }
 }
