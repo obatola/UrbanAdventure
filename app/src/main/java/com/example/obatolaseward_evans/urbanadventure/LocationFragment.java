e3091ffa2bfa445d0d2c6c9381af1a1203c806e8
Oba Seward-Evans <oba.seward@gmail.com>
2017-02-25 08:07:39 -0500

    clean up
    remove unused imports

--- app/src/main/java/com/example/obatolaseward_evans/urbanadventure/LocationFragment.java
+++ app/src/main/java/com/example/obatolaseward_evans/urbanadventure/LocationFragment.java
@@ -1,56 +1,15 @@
 package com.example.obatolaseward_evans.urbanadventure;
 
-import android.app.Activity;
-import android.content.ContentResolver;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Matrix;
-import android.graphics.Paint;
-import android.graphics.RectF;
-import android.graphics.drawable.BitmapDrawable;
-import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.text.Editable;
-import android.text.Html;
-import android.text.TextWatcher;
-import android.text.format.DateFormat;
-import android.util.DisplayMetrics;
-import android.util.Log;
-import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.CompoundButton.OnCheckedChangeListener;
-import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.gms.vision.Frame;
-import com.google.android.gms.vision.face.Face;
-import com.google.android.gms.vision.face.FaceDetector;
-
-import org.w3c.dom.Text;
-
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.util.Date;
 import java.util.UUID;
 
 public class LocationFragment extends Fragment {
@@ -106,7 +65,7 @@ public class LocationFragment extends Fragment {
         type.setText(location.getLocationType().toString());
 
         String description = location.getDescription();
-        des.setText(Html.fromHtml(description));
+        des.setText(description);
 
         String im = "@drawable/" + location.getPicturePath();
 
